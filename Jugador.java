94cfb9316ddca294eb3104dec844b9ca7f955b7f
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-01-23 13:00:07 +0100

    Esqueleto inicial de Jugador y Mano

--- Jugador.java
+++ Jugador.java
@@ -1,6 +1,57 @@
 /**
- * Jugador
+ * Explica tu código aquí
+ * 
+ * @author Adrián Jiménez Santiago
  */
-public class Jugador {
 
+class Jugador {
+
+  // Attributos
+  private Mano mano;
+  private double saldo;
+  private double apuesta;
+
+  public Jugador(double saldoInicial) {
+    mano = new Mano();
+    saldo = saldoInicial;
+    apuesta = 0;
+  }
+
+  // Métodos
+  public void recibirCarta(Carta carta) {
+    mano.recibirCarta(carta);
+  }
+
+  public double realizarApuesta(double monto) {
+    if (monto <= saldo) {
+      apuesta = monto;
+      saldo -= apuesta;
+      return apuesta;
+    } else {
+      System.out.println("No tienes tanto saldo como para apostar esa cantidad.");
+      return 0;
+    }
+  }
+
+  public void incrementarSaldo(double monto) {
+    saldo += monto;
+  }
+
+  public void reiniciarMano() {
+    mano.reiniciar();
+    apuesta = 0;
+  }
+
+  // Getters
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public double getApuesta() {
+    return apuesta;
+  }
+
+  public Mano getMano() {
+    return mano;
+  }
 }
--- Mano.java
+++ Mano.java
@@ -1,6 +1,40 @@
-/**
- * Mano
- */
-public class Mano {
+import java.util.Arrays;
 
+/**
+ * Explica tu código aquí
+ * 
+ * @author Adrián Jiménez Santiago
+ */
+class Mano {
+  private Carta[] cartas;
+  private int numCartas;
+
+  public Mano() {
+    cartas = new Carta[10]; // Como mucho en una mano hay 10 cartas.
+    numCartas = 0;
+  }
+
+  public void recibirCarta(Carta carta) {
+    if (numCartas < 10) {
+      cartas[numCartas++] = carta;
+    }
+  }
+
+  public double calcularPuntuacion() {
+    double puntuacion = 0;
+    for (int i = 0; i < numCartas; i++) {
+      puntuacion += cartas[i].getPuntuacion();
+    }
+    return puntuacion;
+  }
+
+  public Carta[] getCartas() {
+    return Arrays.copyOf(cartas, numCartas); // Utilizando copyOf hacemos una copia exacta del array de cartas junto con
+                                             // su número
+  }
+
+  public void reiniciar() { // Reiniciamos la mano
+    cartas = new Carta[10];
+    numCartas = 0;
+  }
 }
