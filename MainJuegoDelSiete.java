765aa6a5bd5f86e44029189b46bb25cd47065d53
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-01-23 15:41:07 +0100

    Comenzado la Clase main para implementar las funcionalidades de las Clases

--- MainJuegoDelSiete.java
+++ MainJuegoDelSiete.java
@@ -1,6 +1,22 @@
+
 /**
  * MainJuegoDelSiete
  */
-public class MainJuegoDelSiete {
 
+import java.util.Scanner;
+
+public class MainJuegoDelSiete {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Baraja baraja = new Baraja();
+    Jugador jugador = new Jugador(100.00);
+
+    while (true) {
+      System.out.println("########## JUEGO DEL SIETE ##########");
+      System.out.println("Tu sueldo actual es de: " + jugador.getSaldo());
+      System.out.println("¿Cúantos quieres apostar? (Pulse 0 para salir)");
+      double apuesta = sc.nextDouble();
+
+    }
+  }
 }
