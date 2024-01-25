b4885cf6eb31a1649ea83021981da32f4f17e7a4
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-01-25 15:45:05 +0100

    Cambios en el main

--- MainJuegoDelSiete.java
+++ MainJuegoDelSiete.java
@@ -15,7 +15,7 @@ public class MainJuegoDelSiete {
     while (true) {
       System.out.println("########## JUEGO DEL SIETE ##########");
       System.out.println("Tu sueldo actual es de: " + jugador.getSaldo());
-      System.out.println("¿Cúantos quieres apostar? (Pulse 0 para salir)");
+      System.out.println("¿Cúanto quieres apostar? (Pulse 0 para salir)");
       double apuesta = sc.nextDouble();
 
       if (apuesta == 0) {
@@ -39,14 +39,45 @@ public class MainJuegoDelSiete {
           System.out.println("Quieres robar otra carta?  (SI/NO)");
           char respuesta = sc.next().charAt(0);
 
-          if (respuesta == "NO" || respuesta == "no" || jugador.getMano().calcularPuntuacion() >= 7.5) {
+          if (respuesta "NO" || respuesta == "no" || jugador.getMano().calcularPuntuacion() >= 7.5) {
             break;
           }
         }
       }
 
-      // TODO: Turno de la banca --GIOVANNI--
+      // Turno de la banca
+        Mano manoBanca = new Mano();
+        while (manoBanca.calcularPuntuacion() < 7.5) {
+          Carta carta = baraja.repartir();
+          manoBanca.recibirCarta(carta);
+        }
 
+        // Determinar ganador
+        double puntuacionJugador = jugador.getMano().calcularPuntuacion();
+        double puntuacionBanca = manoBanca.calcularPuntuacion();
+
+        System.out.println("Puntuación del jugador: " + puntuacionJugador);
+        System.out.println("Puntuación de la banca: " + puntuacionBanca);
+
+        if (puntuacionJugador > 7.5 || (puntuacionBanca <= 7.5 && puntuacionBanca > puntuacionJugador)) {
+          System.out.println("¡Has perdido! Pierdes " + jugador.getApuesta() + " unidades.");
+          jugador.reiniciarMano();
+        } else {
+          double ganancias = jugador.getApuesta() * 2;
+          System.out.println("¡Has ganado! Ganancias: " + ganancias + " unidades.");
+          jugador.incrementarSaldo(ganancias);
+          jugador.reiniciarMano();
+        }
+
+        // Preguntar si quiere seguir jugando
+        System.out.print("¿Quieres seguir jugando? (S/N): ");
+        char continuar = scanner.next().charAt(0);
+        if (continuar == 'N' || continuar == 'n') {
+          System.out.println("Gracias por jugar. Tu saldo final es: " + jugador.getSaldo());
+          break;
+        }
+      } else {
+        System.out.println("Apuesta no válida. Ingresa un monto entre 1 y " + jugador.getSaldo());
+      }
     }
-  }
 }
