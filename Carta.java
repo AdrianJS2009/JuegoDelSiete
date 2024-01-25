801c50b751d56ab1953bdaeb00292fc0e34d9f0d
TheRealGGIOVI <ggiomes590@g.educaand.es>
2024-01-25 16:46:50 +0100

    Arreglo de valores a las cartas asociadas

--- Carta.java
+++ Carta.java
@@ -12,7 +12,6 @@ class Carta {
     }
 
     public double getPuntuacion() {
-        double numero = 0.0;
         switch (valor) {
             case "1":
             case "2":
@@ -21,17 +20,18 @@ class Carta {
             case "5":
             case "6":
             case "7":
-                numero = Integer.parseInt(valor);
-                break;
-            case "J":
-            case "Q":
-            case "K":
-                numero = 0.5;
-                break;
+                return Double.parseDouble(valor);
+            case "Sota":
+            case "Caballo":
+            case "Rey":
+            case "J": // Para Jota, que asumimos es equivalente a Sota
+            case "Q": // Para Queen, que asumimos es equivalente a Caballo
+            case "K": // Para King, que asumimos es equivalente a Rey
+                return 0.5;
             default:
                 System.out.println("Valor de carta desconocido: " + valor);
+                return 0; // O podrías lanzar una excepción si prefieres manejarlo así
         }
-        return numero;
     }
 
     @Override
