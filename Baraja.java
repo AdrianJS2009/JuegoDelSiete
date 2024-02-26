985004db4bda2ef5f7ff141aad5ce40c65f64f70
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-02-26 21:45:32 +0100

    Refactorizacion de Baraja para utilizar ArrayList en vez de List

--- Baraja.java
+++ Baraja.java
@@ -5,29 +5,29 @@
  * @author Giovanni Giove
 */
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Baraja {
 
     // Atributos
 
-    private Carta[] cartas;
+    private ArrayList<Carta> cartas;
     private int index;
 
     // Constructor
 
     public Baraja() {
-        cartas = new Carta[40];
+        cartas = new ArrayList<>();
         index = 0; // usaremos index a la hora de repartir las cartas
 
         String[] palos = { "Copas", "Bastos", "Oros", "Espadas" };
         String[] valores = { "1", "2", "3", "4", "5", "6", "7", "Sota", "Caballo", "Rey" };
 
-        int i = 0;
-        for (int paloIndex = 0; paloIndex < palos.length; paloIndex++) {
-            for (int valorIndex = 0; valorIndex < valores.length; valorIndex++) {
-                cartas[i] = new Carta(palos[paloIndex], valores[valorIndex]);
-                i++;
+        for (String palo : palos) {
+            for (String valor : valores) {
+
+                cartas.add(new Carta(palo, valor));
             }
         }
     }
@@ -35,20 +35,13 @@ class Baraja {
     // Métodos
 
     public void barajar() {
-        Random numAzar = new Random(); // Utilizo la clase Random para generar números random
-        for (int i = cartas.length - 1; i > 0; i--) {
-            int j = numAzar.nextInt(i + 1); // j es numero random entre 0 y i
-            // Barajamos las cartas
-            Carta aux = cartas[i];
-            cartas[i] = cartas[j];
-            cartas[j] = aux;
-        }
+        Collections.shuffle(cartas);
         index = 0; // Si ponemos indice 0 se reparten las cartas desde el primer objeto
     }
 
     public Carta repartir() {
-        if (index < cartas.length) {
-            return cartas[index++];
+        if (index < cartas.size()) {
+            return cartas.get(index++);
         } else {
             System.out.println("No quedan cartas en la baraja."); // No deberia de ocurrir pero por si acaso.
 
